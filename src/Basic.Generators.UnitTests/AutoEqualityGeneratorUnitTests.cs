5fe98acc9257d2af167e56762b0e8dcbfefbc0fd
Jared Parsons <jared@popcornbear.org>
2023-04-17 08:53:24 -0700

    Equality complexity

--- src/Basic.Generators.UnitTests/AutoEqualityGeneratorUnitTests.cs
+++ src/Basic.Generators.UnitTests/AutoEqualityGeneratorUnitTests.cs
@@ -29,6 +29,7 @@ public class AutoEqualityGeneratorUnitTests
 
         var expected = """
             using System;
+            using System.Collections.Generic;
 
             #nullable enable
 
@@ -60,6 +61,69 @@ public class AutoEqualityGeneratorUnitTests
                     hash.Add(Field);
                     return hash.ToHashCode();
                 }
+
+            }
+            """;
+
+        GeneratorTestUtil.Verify(source, expected, generatedTreeIndex: 1);
+    }
+
+    [Fact]
+    public void OperatorAndEquals()
+    {
+        var source = """
+            namespace Example;
+
+            #pragma warning disable CS0649
+
+            [AutoEquality(true)]
+            partial class Simple
+            {
+                int Field1;
+                object Field2;
+                string Field3;
+            }
+            """;
+
+        var expected = """
+            using System;
+            using System.Collections.Generic;
+
+            #nullable enable
+
+            namespace Example;
+
+            partial class Simple : IEquatable<Simple?>
+            {
+                public static bool operator==(Simple? left, Simple? right) =>
+                    left is not null ? left.Equals(right) : right is null;
+
+                public static bool operator!=(Simple? left, Simple? right) =>
+                    !(left == right);
+
+                public override bool Equals(object? other) =>
+                    other is Simple o && Equals(o);
+
+                public bool Equals(Simple? other)
+                {
+                    if (other is null) 
+                        return false;
+
+                    return
+                        this.Field1 == other.Field1 &&
+                        EqualityComparer<object>.Default.Equals(this.Field2, other.Field2) &&
+                        string.Equals(this.Field3, other.Field3, StringComparison.OrdinalIgnoreCase);
+                }
+
+                public override int GetHashCode()
+                {
+                    var hash = new HashCode();
+                    hash.Add(Field1);
+                    hash.Add(Field1);
+                    hash.Add(Field1);
+                    return hash.ToHashCode();
+                }
+
             }
             """;
 
--- src/Basic.Generators/AutoEqualityGenerator.cs
+++ src/Basic.Generators/AutoEqualityGenerator.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 
@@ -13,16 +14,19 @@ namespace Basic.Generators;
 [Generator(LanguageNames.CSharp)]
 public sealed class AutoEqualityGenerator : IIncrementalGenerator
 {
+    private const string AttributeName = "AutoEqualityAttribute";
+    private const string AttributeMetadataName = AttributeName;
+
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
         context.RegisterPostInitializationOutput(callback =>
         {
             callback.AddSource(
                 "AutoEqualityAttribute.cs",
-                """
+                $$"""
 using System;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
-internal sealed class AutoEqualityAttribute : Attribute
+internal sealed class {{AttributeName}} : Attribute
 {
     public bool CaseInsensitive { get; set; }
 
@@ -34,7 +38,7 @@ internal sealed class AutoEqualityAttribute : Attribute
 
         var results = context.SyntaxProvider
             .ForAttributeWithMetadataName(
-                "AutoEqualityAttribute",
+                AttributeMetadataName,
                 CheckEqualityModel,
                 GetEqualityModel)
             .Where(x => x is not null)
@@ -52,10 +56,20 @@ internal sealed class AutoEqualityAttribute : Attribute
                 return null;
             }
 
+            var attribute = context.Attributes.FirstOrDefault(x => x.AttributeClass?.Name == AttributeName);
+            if (attribute is null)
+            {
+                return null;
+            }
+
+            var caseInsensitive =
+                attribute.ConstructorArguments.Length > 0 &&
+                attribute.ConstructorArguments[0].Value is true;
+
             var fields = symbol
                 .GetMembers()
                 .OfType<IFieldSymbol>()
-                .Select(x => x.Name)
+                .Select(x => new FieldModel(x.Name, x.Type?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat) ?? "", UseOperatorField(x, caseInsensitive)))
                 .ToArray();
 
             return new EqualityModel(
@@ -73,6 +87,7 @@ internal sealed class AutoEqualityAttribute : Attribute
 
             builder.Append($$"""
                 using System;
+                using System.Collections.Generic;
 
                 #nullable enable
                 
@@ -89,21 +104,44 @@ internal sealed class AutoEqualityAttribute : Attribute
                     public override bool Equals(object? other) =>
                         other is {{model.TypeName}} o && Equals(o);
 
-                    public bool Equals({{annotatedName}} other)
-                    {
-                        if (other is null) 
-                            return false;
-
-                        return
 
                 """);
 
+            WriteEquals();
+            WriteGetHashCode();
+
+            builder.Append("""
+                }
+                """);
+
+            context.AddSource($"AutoEquality.{model.Namespace}.{model.TypeName}.Generated.cs", builder.ToString());
+
+            void WriteEquals()
             {
+                builder.Append($$"""
+                        public bool Equals({{annotatedName}} other)
+                        {
+                            if (other is null) 
+                                return false;
+
+                            return
+
+                    """);
+
                 using var _ = indent.Increase(3);
                 for (var i = 0; i < model.Fields.Length; i++)
                 {
-                    var field = model.Fields[0];
-                    builder.Append($"{indent.Value}this.{field} == other.{field}");
+                    var field = model.Fields[i];
+                    var name = field.Name;
+                    var comp = field.CompareKind switch
+                    {
+                        CompareKind.Operator => $"this.{name} == other.{name}",
+                        CompareKind.CaseInsensitive => $"string.Equals(this.{name}, other.{name}, StringComparison.OrdinalIgnoreCase)",
+                        CompareKind.Equals => $"EqualityComparer<{field.TypeFullName}>.Default.Equals(this.{name}, other.{name})",
+                        _ => throw new Exception($"invalid {field.CompareKind}")
+                    };
+
+                    builder.Append($"{indent.Value}{comp}");
                     if (i + 1 == model.Fields.Length)
                     {
                         builder.AppendLine(";");
@@ -113,50 +151,87 @@ internal sealed class AutoEqualityAttribute : Attribute
                         builder.AppendLine(" &&");
                     }
                 }
+
+                builder.Append("""
+                        }
+
+
+                    """);
             }
 
-            builder.Append("""
-                    }
-
-                    public override int GetHashCode()
-                    {
-                        var hash = new HashCode();
-
-                """);
-
+            void WriteGetHashCode()
             {
+                builder.Append($$"""
+                        public override int GetHashCode()
+                        {
+                            var hash = new HashCode();
+
+                    """);
+
                 using var _ = indent.Increase(2);
                 for (var i = 0; i < model.Fields.Length; i++)
                 {
                     var field = model.Fields[0];
-                    builder.AppendLine($"{indent.Value}hash.Add({field});");
+                    builder.AppendLine($"{indent.Value}hash.Add({field.Name});");
                 }
+
+                builder.Append("""
+                            return hash.ToHashCode();
+                        }
+
+
+                    """);
+            }
+        }
+
+        static CompareKind UseOperatorField(IFieldSymbol field, bool caseInsensitive)
+        {
+            if (!(field.Type is { } type))
+            {
+                return CompareKind.Equals;
             }
 
-
-            builder.AppendLine("""
-                        return hash.ToHashCode();
-                    }
-                }
-                """);
-
-            context.AddSource($"AutoEquality.{model.Namespace}.{model.TypeName}.Generated.cs", builder.ToString());
+            return UseOperatorType(type.SpecialType, caseInsensitive);
         }
+
+        static CompareKind UseOperatorType(SpecialType specialType, bool caseInsensitive) =>
+            specialType switch
+            {
+                SpecialType.System_Int16 => CompareKind.Operator,
+                SpecialType.System_Int32 => CompareKind.Operator,
+                SpecialType.System_Int64 => CompareKind.Operator,
+                SpecialType.System_UInt16 => CompareKind.Operator,
+                SpecialType.System_UInt32 => CompareKind.Operator,
+                SpecialType.System_UInt64 => CompareKind.Operator,
+                SpecialType.System_IntPtr => CompareKind.Operator,
+                SpecialType.System_UIntPtr => CompareKind.Operator,
+                SpecialType.System_String => caseInsensitive ? CompareKind.CaseInsensitive : CompareKind.Operator,
+                _ => CompareKind.Equals,
+            };
     }
 }
+
+file enum CompareKind
+{
+    Operator,
+    Equals,
+    CaseInsensitive,
+}
+
+file record struct FieldModel(string Name, string TypeFullName, CompareKind CompareKind);
 
 file sealed class EqualityModel
 {
     internal string Namespace { get; }
     internal string TypeName { get; }
     internal bool IsClass { get; }
-    internal string[] Fields { get; }
+    internal FieldModel[] Fields { get; }
 
     internal EqualityModel(
         string @namespace,
         string typeName,
         bool isClass,
-        string[] fields)
+        FieldModel[] fields)
     {
         Namespace = @namespace;
         TypeName = typeName;
