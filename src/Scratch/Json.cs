98a99936241610c64e61016907eb5d588dd168e9
Jared Parsons <jared@popcornbear.org>
2023-04-18 07:09:04 -0700

    Json

---
+++ src/Scratch/Json.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Demo.Json;
+
+public class BuildResult
+{
+    public bool Passed { get; set; }
+    public string? Commit { get; set; }
+    public int PullRequestId { get; set; }
+}
+
+[JsonSerializable(typeof(BuildResult))]
+internal partial class SourceGenerationContext : JsonSerializerContext
+{
+
+}
+
